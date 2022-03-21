04147567e63f02bee51e5d9b5cd1f8caa3a323d3
ivan100-ivoop <83706102+ivan100-ivoop@users.noreply.github.com>
2022-03-21 19:53:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const child_process = require('child_process');
- const path = require('path');
+const path = require('path');
 const fs = require('fs');
 const ngrok = require('ngrok');
 
